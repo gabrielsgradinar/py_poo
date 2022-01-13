7045d1eee3ee397fb3a12b2f700dc052b63d24d7
Gabriel Gradinar <gabrielgradinar@hotmail.com>
2022-01-13 17:01:07 -0300

    usando metodos estaticos

---
+++ metodo_estatico/erro.py
@@ -0,0 +1,14 @@
+class Erro:
+
+    @staticmethod
+    def protocolo():
+        print('Protocolo com erro')
+
+    
+    @staticmethod
+    def entrada():
+        print("Parâmetros incorretos")
+
+
+Erro.protocolo()
+Erro.entrada()
---
+++ metodo_estatico/my_class.py
@@ -0,0 +1,7 @@
+class MinhaClasse:
+
+    @staticmethod
+    def metodo_estatico():
+        print("Estou no método estático")
+
+MinhaClasse.metodo_estatico()
